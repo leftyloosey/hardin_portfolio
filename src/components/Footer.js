bd892b8a2e86a0fe8ce8654ffa4dbe1128c11ea8
davidhardin <thisgoestodavid@gmail.com>
2022-11-30 22:42:27 -0700

    more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
   return (
     <div className='bg-white fixed bottom-0 left-0 right-0 width-auto shadow-sm'>
       <div className='flex flex-row justify-between'>
-        <div className='flex items-end ml-2'>© 2022_david_hardin</div>
-        <div className='flex items-end mr-5'>
+        <div className='flex items-end ml-2 '>© 2022_david_hardin</div>
+        <div className='flex items-end mr-5 space-x-3'>
           <a href='https://github.com/leftyloosey'>
             <img style={style} src={github} alt='github' />
           </a>
