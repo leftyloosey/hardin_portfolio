17d441ed17c0628a858ab8d9365d4d2eacaca5c3
davidhardin <thisgoestodavid@gmail.com>
2022-11-30 22:31:04 -0700

    touches

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const style = { height: '1.5rem' }
 
 const Footer = () => {
   return (
-    <div className='bg-white fixed bottom-0 left-0 right-0 width-auto shadow-sm sticky top-0'>
+    <div className='bg-white fixed bottom-0 left-0 right-0 width-auto shadow-sm'>
       <div className='flex flex-row justify-between'>
-        <div>© 2022 david hardin</div>
-        <div style={style} className='flex flex-row'>
+        <div>© 2022_david_hardin</div>
+        <div style={style}>
           <img src={github} alt='github' />
           <img src={linkedin} alt='linkedin' />
         </div>
