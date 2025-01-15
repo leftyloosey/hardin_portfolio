5c7048b645eda071b10969807ca71d078c5f8f9d
davidhardin <thisgoestodavid@gmail.com>
2025-01-15 00:12:04 -0700

    fucking scrollbars

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,15 +12,13 @@ const Project = ({ handleClick2, project }) => {
   }
 
   return (
-    <div className='flex flex-col justify-between mt-28 h-96 gap-y-4'>
+    <div className='flex flex-col justify-betwee mt-28 h-96 gap-y-4'>
       <div>
         <img src={img} alt='' className='' />
         <h1 className='pl-1 mb-1'>{name}</h1>
-        <p className='pl-1'>{desc}</p>
+        <p className='pl-1 '>{desc}</p>
       </div>
       <div className='flex flex-col justify-betwee'>
-        <div></div>
-
         <div className='flex flex-col gap-y-1'>
           <button className='hover:text-green-300 border-dotted border-2 hover:border-dashed rounded-full'>
             <a href={gitlink} alt='' target='_blank' rel='noopener noreferrer'>
