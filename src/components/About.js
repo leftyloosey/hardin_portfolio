9ae1f19fade2f24988615c2dc726f1ba806530d9
davidhardin <thisgoestodavid@gmail.com>
2025-01-14 22:09:08 -0700

    flicker

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = ({ isOpen, isOpen2, isOpen3, setIsOpen }) => {
   return (
     <div>
       {isOpen ? (
-        <div className='overflow-scroll flex flex-col mt-24 bg-black text-green-500'>
+        <div className='flex flex-col mt-24 bg-black text-green-500'>
           <div className={`h-auto w-48 rounded-sm`}>
             My Name is David Hardin.<br></br>
             <br></br>Full Stack React + Technical Writer<br></br>Mongo, Express,
