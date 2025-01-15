e858f855b8e95d07af14c352bbd366cf118ede5a
davidhardin <thisgoestodavid@gmail.com>
2025-01-14 20:50:17 -0700

    malaia link:

--- src/components/Work.js
+++ src/components/Work.js
@@ -44,7 +44,7 @@ const Work = ({ handleClick2, isOpen2 }) => {
     {
       name: 'Kate Malaia Portfolio',
       gitlink: 'https://github.com/leftyloosey/bree_uncouth',
-      deploy: 'https://leftyloosey.github.io/bree_uncouth_milosz/',
+      deploy: 'https://leftyloosey.github.io/bree_uncouth/',
       img: green_malaia,
       desc: "React, Tailwind. Professor Malaia's porftolio.",
     },
